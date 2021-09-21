76618be2c3e2dc410a0a648d0107e75f4a5bbf05
majorAndry <majorandry@yahoo.com>
2021-09-21 19:58:39 +0200

    Adds serialization interceptor

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,10 +8,12 @@ import {
   Param,
   Query,
   NotFoundException,
+  UseInterceptors,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
+import { SerializeInterceptor } from '../interceptors/serialize.interceptor';
 
 @Controller('auth')
 export class UsersController {
@@ -22,6 +24,7 @@ export class UsersController {
     this.userService.create(body.email, body.password);
   }
 
+  @UseInterceptors(SerializeInterceptor)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.userService.findOne(parseInt(id));
